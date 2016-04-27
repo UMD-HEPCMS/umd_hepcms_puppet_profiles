f0dfe9baf009016ef5f28dd6645827624f34b085
mnudelli <rmlinetsky@gmail.com>
2016-04-27 19:12:31 -0400

    added the service globus-gridftp-server

--- manifests/osg/gridftp.pp
+++ manifests/osg/gridftp.pp
@@ -2,5 +2,23 @@
 class profile::osg::gridftp {
 
   include ::osg::gridftp
-
+  
+  # ensure that osg-se-hadoo-client is installed
+  package { 'osg-se-hadoop-client':
+    ensure  => 'installed',
+    require => Yumrepo['osg'], # If the package comes from OSG repos, then may also want to include OSG class 
+  }
+   package { 'osg-gridftp':
+    ensure  => 'present',
+  }
+  
+  # ensure the globus-gridftp-server starts
+   service { 'globus-gridftp-server':
+    ensure     => 'running',
+    enable     => true,
+    hasstatus  => true, # TODO: status does not work!
+    hasrestart => true,
+  }
+  
+  
 }
