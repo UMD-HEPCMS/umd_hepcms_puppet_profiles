f3655db562ba5a13aa1bb1663ce36e81737460ca
Wayne Kueh <wayneku88@gmail.com>
2016-05-19 14:22:52 -0400

    Escaped some quotation marks

--- manifests/interactive.pp
+++ manifests/interactive.pp
@@ -4,10 +4,10 @@ class profile::interactive  {
  # wont actually run the command but will check if installed 
  exec { 'yum Group Install':
   
-   unless ('/usr/bin/yum grouplist "Office Suite and Productivity" | /bin/grep "^Installed Groups"') {
+   unless '/usr/bin/yum grouplist \"Office Suite and Productivity\" | /bin/grep \"^Installed Groups\"',
     #command => '/usr/bin/yum -y groupinstall "Office Suite and Productivity"',
-    notify { "Office Suite and Productivity not installed": }
-   }
+   notify { "Office Suite and Productivity not installed": }
+   
   
    unless  => '/usr/bin/yum grouplist "Graphics Creation Tools" | /bin/grep "^Installed Groups"',
    #command => '/usr/bin/yum -y groupinstall "Graphics Creation Tools"',
