f592e8e0c87d67be2812737c84fae121991df9f4
mnudelli <rmlinetsky@gmail.com>
2016-05-19 14:18:25 -0400

    condor and condor-ce added
    packaging update

--- manifests/osg/CE/install.pp
+++ manifests/osg/CE/install.pp
@@ -10,6 +10,15 @@ class profile::osg::ce {
   # condor
   # osg-ce/htcondor
   
+  package { 'condor':
+    ensure => 'present',
+	before => Package['condor-ce'],
+  }
+  package { 'condor-ce':
+    ensure => 'present',
+	name => 'condor-ce',
+  }
+  
   
   
   
