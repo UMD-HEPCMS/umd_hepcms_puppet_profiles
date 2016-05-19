94528d2b318ec692d3b636b02bc9ed52bceeb508
mnudelli <rmlinetsky@gmail.com>
2016-05-19 13:55:44 -0400

    added configurations for CE
    ce::configure
    ce::install
    ce::service

---
+++ manifests/osg/CE/configure.pp
@@ -0,0 +1,20 @@
+# == Class: profile::osg::ce::configure
+# This file will be used to setup the configurations of the compute element
+class profile::osg::ce::configure {
+
+  
+  include profile::osg 
+  include osg::cacerts
+  
+  # Files required will be
+  # http cert
+  
+  # httpcert.pem
+  # httpkey
+  
+  
+  
+  
+  
+
+}
---
+++ manifests/osg/CE/install.pp
@@ -0,0 +1,17 @@
+# == Class: profile::osg::ce::install
+class profile::osg::ce {
+
+  #include ::osg::ce
+  include profile::osg 
+  include osg::cacerts
+  
+  # installation puppet file for CE 
+  # packages:
+  # condor
+  # osg-ce/htcondor
+  
+  
+  
+  
+
+}
