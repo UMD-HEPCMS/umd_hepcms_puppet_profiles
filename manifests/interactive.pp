1ca4aae681d25f0fae550290334e1726c097b187
karaethonWadjet <whkzyx42666@gmail.com>
2016-05-19 14:59:28 -0400

    changed notify to echo commands

--- manifests/interactive.pp
+++ manifests/interactive.pp
@@ -4,32 +4,33 @@ class profile::interactive  {
  # wont actually run the command but will check if installed 
  exec { 'Office Suite and Productivity':
   #command => '/usr/bin/yum -y groupinstall "Office Suite and Productivity"',
+  command => ['echo "Office Suite and Productivity not installed"',
+  	     	   'echo "uh oh"'],
   onlyif => '/usr/bin/yum grouplist "Office Suite and Productivity" | /bin/grep "^Installed Groups"',
-  notify {"Office Suite and Productivity not installed":},
  }
  exec { 'Graphics Creation Tools':
   #command => '/usr/bin/yum -y groupinstall "Graphics Creation Tools"',
+  command => 'echo "Graphics Creation Tools  not installed"',
   onlyif => '/usr/bin/yum grouplist "Graphics Creation Tools" | /bin/grep "^Installed Groups"',
-  notify => "Graphics Creation Tools  not installed",
  }
  exec { 'Desktop':
   #command => '/usr/bin/yum -y groupinstall "Desktop"',
+  command => 'echo "Desktop not installed"',
   onlyif => '/usr/bin/yum grouplist "Desktop" | /bin/grep "^Installed Groups"',
-  notify => "Desktop not installed",
  }
  exec { 'X Window System': 
-   #command => '/usr/bin/yum -y groupinstall "X Window System"',
-   onlyif => '/usr/bin/yum grouplist "X Window System" | /bin/grep "^Installed Groups"',
-   notify => "X Window System  not installed",
+  #command => '/usr/bin/yum -y groupinstall "X Window System"',
+  command => 'echo "X Window System  not installed"',
+  onlyif => '/usr/bin/yum grouplist "X Window System" |/bin/grep "^Installed Groups"',
  }
  exec { 'TeX Support': 
-   #command => '/usr/bin/yum -y groupinstall "TeX support"',
-   onlyif => '/usr/bin/yum grouplist "TeX support" | /bin/grep "^Installed Groups"',
-   notify => "TeX Support not installed",
+  #command => '/usr/bin/yum -y groupinstall "TeX support"',
+  command => ''echo "TeX Support not installed"',
+  onlyif => '/usr/bin/yum grouplist "TeX support" | /bin/grep "^Installed Groups"',
  }
  exec { 'Desktop Platform Development':
   #command => '/usr/bin/yum -y groupinstall "Desktop Platform Development"',
+  command => 'echo "Desktop Platform Development not installed"',
   onlyif => '/usr/bin/yum grouplist "Desktop Platform Development" | /bin/grep "^Installed Groups"',
-  notify => "Desktop Platform Development not installed",
  }
 }
