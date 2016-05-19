219b94bc1d2088d1008e968d3ed9b049eb67b744
mnudelli <rmlinetsky@gmail.com>
2016-05-19 13:04:43 -0400

    Created a repository url puppet class
    repo puppet class

--- manifests/osg/ce.pp
+++ manifests/osg/ce.pp
@@ -2,5 +2,6 @@
 class profile::osg::ce {
 
   include ::osg::ce
+  
 
 }
---
+++ manifests/osg/repos.pp
@@ -0,0 +1,16 @@
+# Yum Repository File for OSG3.3
+class profile::osg::repos {
+	# including the profile module 
+	include profiles::osg 
+	# include the params file, which will initialize things
+	include profiles::osg::params 
+	
+	$baseurls = {
+		'osg' => "https://repo.grid.iu.edu/osg/3.3/osg-3.3-el6-release-latest.rpm"
+	}
+	yumrepo { 'osg':
+	 baseurl    => $baseurls['osg'],
+	 descr      => "OSG Software for Scientific Linux 6.7",
+     enabled    => '1',
+	}
+}
