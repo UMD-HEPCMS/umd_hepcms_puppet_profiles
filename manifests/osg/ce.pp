0aa7a8c69d933d230b2604ab903a29b64548ea67
mnudelli <rmlinetsky@gmail.com>
2016-05-20 20:22:27 -0400

    included the includes

--- manifests/osg/ce.pp
+++ manifests/osg/ce.pp
@@ -1,11 +1,10 @@
+#Initialization of facts for the Compute Element
 # == Class: profile::osg::ce
 class profile::osg::ce {
 
-  #include ::osg::ce
+  include ::osg::ce
   include profile::osg 
   include osg::cacerts
-  
-  #Initialization of facts for the Compute Element
   
   
   
