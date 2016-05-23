4f9b968f8eee7e5fd1317393ca60de86acbf1d9d
mnudelli <rmlinetsky@gmail.com>
2016-05-23 13:24:24 -0400

    removed anchor
    removed anchor

--- manifests/osg/ce.pp
+++ manifests/osg/ce.pp
@@ -5,17 +5,6 @@ class profile::osg::ce {
   include ce
   include profile::osg 
   include osg::cacerts
-  
-  # test anchor 
- # anchor { 'osg::ce::start': }->
- # Class['osg']->
- # Class['osg::cacerts']->
- # class { 'osg::ce::install': }->
- # Class['osg::gridftp']->
- # class { 'osg::ce::config': }->
- # class { 'osg::ce::service': }->
- # anchor { 'osg::ce::end': }
-  
-  
+    
 
 }
