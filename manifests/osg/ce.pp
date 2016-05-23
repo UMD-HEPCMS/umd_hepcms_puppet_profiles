cac0ed4cc234a89b7bab2329c1465e3b68d16d11
mnudelli <rmlinetsky@gmail.com>
2016-05-23 13:16:05 -0400

    changed the included packages

--- manifests/osg/ce.pp
+++ manifests/osg/ce.pp
@@ -2,10 +2,19 @@
 # == Class: profile::osg::ce
 class profile::osg::ce {
 
-  include ::osg::ce
+  include ce
   include profile::osg 
   include osg::cacerts
   
+  # test anchor 
+ # anchor { 'osg::ce::start': }->
+ # Class['osg']->
+ # Class['osg::cacerts']->
+ # class { 'osg::ce::install': }->
+ # Class['osg::gridftp']->
+ # class { 'osg::ce::config': }->
+ # class { 'osg::ce::service': }->
+ # anchor { 'osg::ce::end': }
   
   
 
