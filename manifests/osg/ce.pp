96e4f14b5fad592cd59d0d131181d8a316c35f06
mnudelli <rmlinetsky@gmail.com>
2016-05-25 12:37:28 -0400

    added the tomcat user

--- manifests/osg/ce.pp
+++ manifests/osg/ce.pp
@@ -1,7 +1,18 @@
 class profile::osg::ce {
 
   
-  #include profile::osg 
+  
   include osg::cacerts
     
+   user { 'tomcat':
+    ensure     => 'present',
+    name       => 'tomcat',
+    gid        => tomcat,
+    home       => '/home/tomcat', # wherever package puts $HOME for user
+    shell      => '/bin/bash', # /bin/bash or /sbin/nologin
+    system     => true,
+    comment    => 'hdfs user synchronized across all systems',
+    managehome => false,
+    forcelocal => true
+  }
 }
