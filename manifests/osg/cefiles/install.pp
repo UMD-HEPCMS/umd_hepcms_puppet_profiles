db6bc704f66aa8012276713f6f095adee18473d9
mnudelli <rmlinetsky@gmail.com>
2016-05-26 01:01:47 -0400

    added comments

--- manifests/osg/cefiles/install.pp
+++ manifests/osg/cefiles/install.pp
@@ -4,11 +4,12 @@ class profile::osg::cefiles::install {
  
   include osg::cacerts
   
-  
+  # Installing Condor Package, Ensure that it is present
   package { 'condor':
     ensure => 'present',
 	before => Package['osg-ce-condor'],
   }
+  # HTCondorCE Package
   package { 'osg-ce-condor':
     ensure => 'present',
 	name => 'osg-ce-condor',
