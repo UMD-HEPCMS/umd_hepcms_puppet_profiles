10878a501c41c0620ed3027bc5b1e26be61b59a2
mnudelli <rmlinetsky@gmail.com>
2016-05-26 01:44:54 -0400

    added an ensure condor/config.d directory

--- manifests/osg/cefiles/configure.pp
+++ manifests/osg/cefiles/configure.pp
@@ -30,16 +30,24 @@ class profile::osg::cefiles::configure {
     require => File['/etc/grid-security/http'],
   }
   
+  
+  # ensure condor/config.d exists
+  file { '/etc/condor/config.d':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
   file { '/etc/condor/config.d/00-restart_peaceful.config':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/00-restart_peaceful.config',
-    require => File['/etc/condor/config.d/00-restart_peaceful.config'],
+    require => File['/etc/condor/config.d'],
   }
   
   file { '/etc/condor/config.d/10-batch_gahp_blahp.config':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/10-batch_gahp_blahp.config',
-    require => File['/etc/condor/config.d/10-batch_gahp_blahp.config'],
+    require => File['/etc/condor/config.d'],
   }
   
   file { '/etc/condor/config.d/50-condor-ce-defaults.conf':
@@ -48,18 +56,18 @@ class profile::osg::cefiles::configure {
     group   => 'root',
     mode    => '0400',
     source  => 'file:///data/site_conf/condor/50-condor-ce-defaults.conf',
-    require => File['/etc/condor/config.d/50-condor-ce-defaults.conf'],
+    require => File['/etc/condor/config.d'],
   }
   
   file { '/etc/condor/config.d/99-condor-ce.conf':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/99-condor-ce.conf',
-    require => File['/etc/condor/config.d/99-condor-ce.conf'],
+    require => File['/etc/condor/config.d/'],
   }
   file { '/etc/condor/config.d/99_gratia.conf':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/99_gratia.conf',
-    require => File['/etc/condor/config.d/99_gratia.conf'],
+    require => File['/etc/condor/config.d/'],
   }
   
   file { '/etc/condor/config.d/cluster.conf':
