eb77d52051b582273e20148188bd4a7f20deaed4
mnudelli <rmlinetsky@gmail.com>
2016-05-26 01:12:59 -0400

    added condor configuration files
    added condor configuration files

--- manifests/osg/cefiles/configure.pp
+++ manifests/osg/cefiles/configure.pp
@@ -30,5 +30,64 @@ class profile::osg::cefiles::configure {
     require => File['/etc/grid-security/http'],
   }
   
+  file { '/etc/condor/config.d/00-restart_peaceful.config':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/00-restart_peaceful.config',
+    require => File['/etc/condor/config.d/00-restart_peaceful.config'],
+  }
   
+  file { '/etc/condor/config.d/10-batch_gahp_blahp.config':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/10-batch_gahp_blahp.config',
+    require => File['/etc/condor/config.d/10-batch_gahp_blahp.config'],
+  }
+  
+  file { '/etc/condor/config.d/50-condor-ce-defaults.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/50-condor-ce-defaults.conf',
+    require => File['/etc/condor/config.d/50-condor-ce-defaults.conf'],
+  }
+  
+  file { '/etc/condor/config.d/99-condor-ce.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/99-condor-ce.conf',
+    require => File['/etc/condor/config.d/99-condor-ce.conf'],
+  }
+  file { '/etc/condor/config.d/99_gratia.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/99_gratia.conf',
+    require => File['/etc/condor/config.d/99_gratia.conf'],
+  }
+  
+  file { '/etc/condor/config.d/cluster.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/cluster.conf',
+    require => File['/etc/condor/config.d/cluster.conf'],
+  }
+  file { '/etc/condor/config.d/local.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/condor/local.conf',
+    require => File['/etc/condor/config.d/local.conf'],
+  }
 }
