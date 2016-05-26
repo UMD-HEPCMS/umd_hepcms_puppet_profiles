a799a446f3f285a4c5252c7815db755b6fdfc675
mnudelli <rmlinetsky@gmail.com>
2016-05-26 01:27:12 -0400

    debugging file permissions

--- manifests/osg/cefiles/configure.pp
+++ manifests/osg/cefiles/configure.pp
@@ -32,18 +32,12 @@ class profile::osg::cefiles::configure {
   
   file { '/etc/condor/config.d/00-restart_peaceful.config':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/00-restart_peaceful.config',
     require => File['/etc/condor/config.d/00-restart_peaceful.config'],
   }
   
   file { '/etc/condor/config.d/10-batch_gahp_blahp.config':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/10-batch_gahp_blahp.config',
     require => File['/etc/condor/config.d/10-batch_gahp_blahp.config'],
   }
@@ -59,34 +53,22 @@ class profile::osg::cefiles::configure {
   
   file { '/etc/condor/config.d/99-condor-ce.conf':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/99-condor-ce.conf',
     require => File['/etc/condor/config.d/99-condor-ce.conf'],
   }
   file { '/etc/condor/config.d/99_gratia.conf':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/99_gratia.conf',
     require => File['/etc/condor/config.d/99_gratia.conf'],
   }
   
   file { '/etc/condor/config.d/cluster.conf':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/cluster.conf',
     require => File['/etc/condor/config.d/cluster.conf'],
   }
   file { '/etc/condor/config.d/local.conf':
     ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
     source  => 'file:///data/site_conf/condor/local.conf',
     require => File['/etc/condor/config.d/local.conf'],
   }
