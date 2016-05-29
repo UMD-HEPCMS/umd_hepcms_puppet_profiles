9146802c216200e5363963deb5bf098a9e24f234
mnudelli <rmlinetsky@gmail.com>
2016-05-29 16:55:51 -0400

    osg-configure exec removed
    this step is taking from different osg module

--- manifests/osg/cefiles/configure.pp
+++ manifests/osg/cefiles/configure.pp
@@ -280,11 +280,4 @@ class profile::osg::cefiles::configure {
     require => File['/etc/osg/config.d/'],
   }
   
-  # run the osg-configure step, first the -v option, then the -c
-  exec { 'osg-configure':
-    path        => ['/usr/bin','/bin','/usr/sbin','/sbin'],
-    command     => '/usr/sbin/osg-configure -c',
-    onlyif      => ['test -f /usr/sbin/osg-configure', '/usr/sbin/osg-configure -v'],
-    refreshonly => true,
-  }
 }
