0de5e880e18e370276b77480713d1d136933546d
mnudelli <rmlinetsky@gmail.com>
2016-06-15 22:49:48 -0400

    makes sure bestman2 service keeps running

--- manifests/osg/sefiles/services.pp
+++ manifests/osg/sefiles/services.pp
@@ -1,4 +1,9 @@
 # == Class: profile::osg::se
 class profile::osg::sefiles::services {
-
+service { 'bestman2':
+    ensure     => 'running',
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+  }
 }
