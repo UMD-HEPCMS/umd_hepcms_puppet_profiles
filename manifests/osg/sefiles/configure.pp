e0e73fb195e41910d652bab85dcee8fccf2c2b0c
mnudelli <rmlinetsky@gmail.com>
2016-06-15 22:50:53 -0400

    TODO: add files

--- manifests/osg/sefiles/configure.pp
+++ manifests/osg/sefiles/configure.pp
@@ -1,6 +1,15 @@
 # == Class: profile::osg::sefiles::configure
 class profile::osg::sefiles::configure  {
 
- 
+#/etc/conf/hdfs-site.xml
+#/etc/conf/core-site.xml
+
+#hostkey and hostcert files
+#/etc/grid-security/bestman
+#/etc/grid-security/bestman/bestmancert.pem
+#/etc/grid-security/bestman/bestmankey.pem
+#/etc/sysconfig/bestman2
+#/etc/bestman2/conf/bestman2.rc
+#/var/log/bestman2 directory
 
 }
