ea89b88ad1f9436000f52e7b172a4b6a5cde92cb
mtonjes <mtonjes@umd.edu>
2016-06-17 16:27:30 -0400

    remove declaration dupe

--- manifests/osg/gftp/install.pp
+++ manifests/osg/gftp/install.pp
@@ -4,10 +4,10 @@ class profile::osg::gftp::install {
   include ::osg::gridftp
   
   # ensure that osg-se-hadoop-client is installed
-  package { 'osg-se-hadoop-client':
+/*  package { 'osg-se-hadoop-client':
     ensure  => 'installed',
     require => Yumrepo['osg'], # If the package comes from OSG repos, then may also want to include OSG class 
-  }
+  } */
    package { 'osg-gridftp':
     ensure  => 'present',
   }
