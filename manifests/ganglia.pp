c4bbe63eb8b054fddb18e4b8d238a809067d3a2e
mnudelli <rmlinetsky@gmail.com>
2016-06-17 21:26:31 -0400

    updated ganglia.pp

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -1,2 +1,21 @@
 class profile::ganglia {
+
+  package { 'ganglia-gmond':
+    ensure => 'present',
+  	before => Service['gmond'],
+  }
+  # pull file from /data
+  file { '/etc/ganglia/gmond.conf':
+    ensure => file,
+    source => 'file:////data/site_conf/ganglia/gmond.conf',
+    require => Package['ganglia-gmond'],
+  }
+ 
+  service {'gmond ':
+   ensure => 'running',
+   enable => true,
+   hasstatus => true,
+   hasrestart => true,
+   require => File['/etc/ganglia/gmond.conf'],
+  }  
 }
