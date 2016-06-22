50455b6f821709611942cf7416c8f1eb29f8d60d
mnudelli <rmlinetsky@gmail.com>
2016-06-21 20:51:45 -0400

    TO DO: add the rest of the files
    back up configuration files to the /data area

--- manifests/osg/sefiles/configure.pp
+++ manifests/osg/sefiles/configure.pp
@@ -4,12 +4,48 @@ class profile::osg::sefiles::configure  {
 #/etc/conf/hdfs-site.xml
 #/etc/conf/core-site.xml
 
-#hostkey and hostcert files
-#/etc/grid-security/bestman
-#/etc/grid-security/bestman/bestmancert.pem
-#/etc/grid-security/bestman/bestmankey.pem
+
+
 #/etc/sysconfig/bestman2
 #/etc/bestman2/conf/bestman2.rc
 #/var/log/bestman2 directory
-
+  
+  # ensure the grid-security directory exists
+  file { '/etc/grid-security':
+   ensure => directory,
+  }
+  # ensure bestman directory exists
+  file { '/etc/grid-security/bestman':
+   ensure => directory,
+  }
+  # ensure host cert
+  file { '/etc/grid-security/hostcert.pem':
+   ensure => file,
+   owner => root,
+   source  => 'file:///data/site_conf/sefiles/hostcert.pem',
+  }
+  # ensure hostkey 
+  file { '/etc/grid-security/hostkey.pem':
+   ensure => file,
+   owner => root,
+   source  => 'file:///data/site_conf/sefiles/hostkey.pem',
+  }
+  # ensure this file exists
+  file { '/etc/grid-security/bestman/bestmancert.pem':
+    ensure  => 'file',
+    owner   => 'bestman',
+    group   => 'bestman',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/sefiles/bestman/bestmancert.pem',
+    
+  }
+  # ensure this file exists
+  file { '/etc/grid-security/bestman/bestmankey.pem':
+    ensure  => 'file',
+    owner   => 'bestman',
+    group   => 'bestman',
+    mode    => '0400',
+    source  => 'file:///data/site_conf/sefiles/bestman/bestmankey.pem',
+    
+  }
 }
