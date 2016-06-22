e74db0322e533879f0cba03fcdc91a10c0794c17
mnudelli <rmlinetsky@gmail.com>
2016-06-21 23:48:57 -0400

    added condor service

--- manifests/int_submit/services.pp
+++ manifests/int_submit/services.pp
@@ -1,6 +1,11 @@
 # Condor Submit Node Services
 class profile::int_submit::services {
 
- 
+ service {'condor':
+   ensure => 'running',
+   enable => true,
+   hasstatus => true,
+   hasrestart => true,
+  }
 
 }
