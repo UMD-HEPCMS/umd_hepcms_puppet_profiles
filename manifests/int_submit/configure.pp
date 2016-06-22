ce0f5ef7760f61dc4475bf9e699fc8743fde2696
mnudelli <rmlinetsky@gmail.com>
2016-06-22 00:03:06 -0400

    added files for condor submit node

--- manifests/int_submit/configure.pp
+++ manifests/int_submit/configure.pp
@@ -2,4 +2,19 @@
 class profile::int_submit::configure {
 
 
+file { '/etc/condor/config.d':
+  ensure => 'directory',
+}
+
+file { '/etc/condor/config.d/local.conf':
+   ensure => file,
+   contents => "DAEMON_LIST = MASTER, SCHEDD",
+}
+
+file { '/etc/condor/config.d/cluster.conf':
+    ensure  => 'file',
+    source  => 'file:///data/site_conf/condor/cluster.conf',
+    require => File['/etc/condor/config.d/'],
+  }
+   
 }
