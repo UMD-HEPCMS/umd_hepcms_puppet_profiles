46162d7d40cc822a53972f8d024f523049dd5bbb
Margarita Nudel-Linetsky <rmlinetsky@gmail.com>
2016-06-22 00:28:32 -0400

    added general software install class for interactive

---
+++ manifests/int_general/install.pp
@@ -0,0 +1,48 @@
+# Installation of General Software
+class profile::int_general::install {
+
+ package {'gedit':
+  ensure => installed,
+ }
+ package {'finger':
+  ensure => installed,
+ }
+ 
+ package {'firefox':
+  ensure => installed,
+ }
+
+ package {'gv':
+  ensure => installed,
+ }
+ 
+ package {'tkinter':
+  ensure => installed,
+ }
+ package {'git-all':
+  ensure => installed,
+ }
+ package {'libXaw':
+  ensure => installed,
+ }
+ 
+ package {'Xaw3d':
+  ensure => installed,
+ }
+ 
+ package {'Xaw3d-devel':
+  ensure => installed,
+ }
+ 
+  package {'libXi':
+  ensure => installed,
+ }
+ 
+  package {'tmux':
+  ensure => installed,
+ }
+ 
+ package {'screen':
+  ensure => installed,
+ }
+}
