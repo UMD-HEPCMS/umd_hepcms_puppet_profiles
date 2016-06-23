e96906f636ac14fe751d13e8102908521da246c7
mtonjes <mtonjes@umd.edu>
2016-06-23 15:01:25 -0400

    install rsv

--- manifests/osg/cefiles/install.pp
+++ manifests/osg/cefiles/install.pp
@@ -17,5 +17,8 @@ class profile::osg::cefiles::install {
   }
   # RSV package for monitoring (doesn't have to be on the same node)
   # https://twiki.opensciencegrid.org/bin/view/Documentation/Release3/InstallRSV
-
+  package { 'rsvr':
+    ensure => 'present',
+	name => 'rsv',
+  }
 }
