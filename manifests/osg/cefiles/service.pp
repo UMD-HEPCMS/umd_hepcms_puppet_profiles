37c23fa52661e7ab09e0dce660b5cfb409b4e60f
mnudelli <rmlinetsky@gmail.com>
2016-06-23 20:26:52 -0400

    rsv, condor-cron and httpd services added

--- manifests/osg/cefiles/service.pp
+++ manifests/osg/cefiles/service.pp
@@ -22,6 +22,25 @@ class profile::osg::cefiles::service {
    hasstatus => true,
    hasrestart => true,
   }
+  service { 'httpd':
+   ensure => 'running',
+   enable => true,
+   hasstatus => true,
+   hasrestart => true,
+  }
+ service { 'condor-cron':
+   ensure => 'running',
+   enable => true,
+   hasstatus => true,
+   hasrestart => true,
+  }
+ service { 'rsv':
+   ensure => 'running',
+   enable => true,
+   hasstatus => true,
+   hasrestart => true,
+  }
+
 # insert something here to make sure puppet does not auto run..
   exec { 'kill-puppet':
    path => ['/bin', '/sbin/'],
