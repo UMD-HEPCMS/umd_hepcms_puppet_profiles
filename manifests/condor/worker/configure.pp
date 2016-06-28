e798a4b89f983045121da1cf7fd599d9f08a9861
mnudelli <mnudelli@users.noreply.github.com>
2016-06-27 20:46:05 -0400

    fixed syntax error in directory path

--- manifests/condor/worker/configure.pp
+++ manifests/condor/worker/configure.pp
@@ -9,12 +9,12 @@ file { '/etc/condor/config.d':
 file { '/etc/condor/config.d/cluster.conf':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/cluster.conf',
-    require => File['/etc/condor-worker/config.d/'],
+    require => File['/etc/condor/config.d/'],
 }
 file { '/etc/condor/config.d/local.conf':
     ensure  => 'file',
     source  => 'file:///data/site_conf/condor/local.conf',
-    require => File['/etc/condor-worker/config.d/'],
+    require => File['/etc/condor/config.d/'],
 } 
 
 }
