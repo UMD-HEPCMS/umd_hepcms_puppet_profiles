19b5587f8997e86ce16944282b567c4f06f4405b
mtonjes <mtonjes@umd.edu>
2016-07-08 14:57:53 -0400

    not just filename
    have to have it right inside the file too

--- manifests/osg/sefiles/service.pp
+++ manifests/osg/sefiles/service.pp
@@ -1,5 +1,5 @@
 # == Class: profile::osg::se
-class profile::osg::sefiles::services {
+class profile::osg::sefiles::service {
 service { 'bestman2':
     ensure     => 'running',
     enable     => true,
