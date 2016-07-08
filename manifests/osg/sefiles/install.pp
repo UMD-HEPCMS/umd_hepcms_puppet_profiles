b52e0b69a19e48f0d913d31842176d7eb5ca475c
mtonjes <mtonjes@umd.edu>
2016-07-08 14:52:14 -0400

    put the cron job in puppet
    try the cron job in puppet instead

--- manifests/osg/sefiles/install.pp
+++ manifests/osg/sefiles/install.pp
@@ -4,5 +4,15 @@ class profile::osg::sefiles::install {
  package { 'osg-se-bestman':
     ensure  => 'installed',
   }
+## restart the service every Saturday 1:15am cron job - per US CMS T3 recommendation
+cron { 'restart bestman2':
+  command => '/sbin/service bestman2 restart',
+  user    => 'root',
+  hour    => '1',
+  minute  => '15',
+  date    => '*',
+  month   => '*',
+  weekday => '6',
+}
 
 }
