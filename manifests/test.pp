89e2c34bf6ad19df093f5f66a8da21b92fbef41a
mnudelli <mnudelli@users.noreply.github.com>
2016-07-19 22:09:28 -0400

    changed sudo

--- manifests/test.pp
+++ manifests/test.pp
@@ -6,10 +6,9 @@ class profile::test {
     ensure => 'file',
     content  => template('profile/test.erb'),
   }
-  sudo::conf { 'sudoers-bestman':
+  sudo::conf { 'bestman':
    ensure => present,
-   source => 'file:///data/site_conf/sefiles/bestman/sudoers-bestman',
-   sudo_file_name  => 'sudoers-bestman',
+   content => 'template('profile/bestman.sudo.erb')',
  }
   
 }
