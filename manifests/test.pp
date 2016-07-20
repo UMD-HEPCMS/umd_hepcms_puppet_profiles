011702620104b73835ca6fab89f15575be53eeb2
mnudelli <mnudelli@users.noreply.github.com>
2016-07-19 20:57:46 -0400

    added sudoers for testing

--- manifests/test.pp
+++ manifests/test.pp
@@ -6,6 +6,10 @@ class profile::test {
     ensure => 'file',
     content  => template('profile/test.erb'),
   }
- 
+  sudo::conf { 'sudoers-bestman':
+   ensure => present,
+   source => 'file:///data/site_conf/sefiles/bestman/sudoers-bestman',
+   sudo_file_name  => 'sudoers-bestman',
+ }
   
 }
