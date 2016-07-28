9f92dfe46f4bf2480ee28500807b36ba9e89e213
mtonjes <mtonjes@umd.edu>
2016-07-28 10:55:44 -0400

    pick up conf from condor-worker

--- manifests/condor/worker/configure.pp
+++ manifests/condor/worker/configure.pp
@@ -8,12 +8,12 @@ file { '/etc/condor/config.d':
 
 file { '/etc/condor/config.d/cluster.conf':
     ensure  => 'file',
-    source  => 'file:///data/site_conf/condor/cluster.conf',
+    source  => 'file:///data/site_conf/condor-worker/cluster.conf',
     require => File['/etc/condor/config.d/'],
 }
 file { '/etc/condor/config.d/local.conf':
     ensure  => 'file',
-    source  => 'file:///data/site_conf/condor/local.conf',
+    source  => 'file:///data/site_conf/condor-worker/local.conf',
     require => File['/etc/condor/config.d/'],
 } 
 
