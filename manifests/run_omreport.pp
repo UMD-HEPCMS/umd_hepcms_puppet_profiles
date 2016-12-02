03eb2c814830dca44126ccb1f9f2ac2ee9578044
jrtaylor95 <jrtaylor95@gmail.com>
2016-12-01 22:21:34 -0500

    Update run_omreport.pp

--- manifests/run_omreport.pp
+++ manifests/run_omreport.pp
@@ -1,6 +1,6 @@
 class profile::run_omreport {
   cron { 'get_omsa_reports':
-    command => '/usr/bin/bash /data/monitoring/scripts/run_omreport.sh',
+    command => '/bin/bash /data/monitoring/scripts/run_omreport.sh',
     user    => 'root',
     hour    => 22,
     minute  => 0,
